04347f1131d8cc6242366eb5c67437059abf0c68
CadenC01 <93016479+CadenC01@users.noreply.github.com>
2023-12-03 22:36:02 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -3,40 +3,22 @@ import Tickets from "./componets/Tickets";
 import Admin from "./componets/Admin";
 import Login from "./componets/Login";
 import Navbar from "./componets/Navbar";
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div>
-        <div> <Navbar /> </div>
-            <Routes>
-                <Route
-                    exact
-                    path="/"
-                    element={<Login />}
-                />
-                <Route
-                    exact
-                    path="/Admin"
-                    element={<Admin />}
-                />
-                <Route
-                    exact
-                    path="/Tickets"
-                    element={<Tickets />}
-                />
-            </Routes>
-        
+      <div>
+        {" "}
+        <Navbar />{" "}
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/Admin" element={<Admin />} />
+        <Route exact path="/Tickets" element={<Tickets />} />
+      </Routes>
     </div>
-);
-
-
+  );
 }
 
 export default App;
--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,21 +1,19 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <a href="/">Login</a>
-                </li>
-                <li>
-                    <a href="/Tickets">Tickets</a>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div id="navcont">
+      <ul>
+        <li>
+          <a href="/">Login</a>
+        </li>
+        <li>
+          <a href="/Tickets">Tickets</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
